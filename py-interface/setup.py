2211efc36d59c0a91857d30a5f88bf6b2805600a
Parth Raut <praut@umich.edu>
2024-09-26 15:19:39 -0400

    Add py-interface for version 1.0.0+rocm-5.5.0

--- py-interface/setup.py
+++ py-interface/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='amdsmi',
-    version='0.1',
+    version='1.0.0+rocm-5.5.0',
     description="SMI LIB - AMD GPU Monitoring Library",
     long_description=long_description,
     long_description_content_type="text/markdown",
