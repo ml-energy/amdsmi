0cf4b1362962b7b78641df682b4935a115b4e00c
Parth Raut <praut@umich.edu>
2024-09-26 22:31:08 -0400

    updated script to go by releases and not tags

--- scripts/fix.sh
+++ scripts/fix.sh
@@ -2,39 +2,21 @@
 
 # Check if a tag is already published in this repo.
 function tag_already_published {
-    tag_name=$1
-
-    # Check if the name contains "so_ver"
-    if echo "$tag_name" | grep -q "so_ver"; then
-        return  # Exit the function if "so_ver" is found
-    fi
+    version_number=$1
 
     # get current tags
     current_tags=$(git tag)
 
-    if echo "$tag_name" | grep -q "pkg_ver"; then
-        version=${tag_name#*-}  # Correctly extract version from tag_name
-
-        # Match the exact tag (whole word match)
-        if echo "$current_tags" | grep -wq "^$version$"; then
-            return 0  # Tag already published (success)
-        else
-            return 1  # No exact match found (failure)
-        fi
+    if echo "$current_tags" | grep -q "$version_number"; then
+        return 0 # Tag already published (success)
     else
-        # must be a rocm version. If rocm is found in any tag as a local version specifier, 
-        # then it has already been published.
-        if echo "$current_tags" | grep -q "$tag_name"; then
-            return 0 # Tag already published (success)
-        else
-            return 1 # No match found
-        fi
+        return 1 # No match found
     fi
 }
 
 # Generate python wrapper. Change path of rocm library and move files into one directory.
 function fix_amdsmi_version {
-    # requires amdsmi version
+
     local version=$1
     cd py-interface
 
@@ -68,27 +50,14 @@ function fix_amdsmi_version {
 
 # Called when there is a new version of amdsmi.
 function clone_and_fix_amdsmi {
-    local name=$1
-    local commit=$2
+    local commit=$1
+    local version=$2
 
     git clone https://github.com/ROCm/amdsmi.git
     cd amdsmi
 
     # checkout the commit
     git checkout $commit
-
-    local version=""
-    local amdsmi_version=""
-
-    # if name begins with "rocm", find amdsmi version
-    if echo "$name" | grep -q "rocm"; then
-        amdsmi_version=$(grep 'get_package_version_number' CMakeLists.txt | awk -F '"' '{print $2}')
-        version="${amdsmi_version}+$name"
-    else
-        # name looks like "amdsmi_pkg_ver-24.6.0", extract out the version number
-        amdsmi_version=${name#*-}
-        version=${name#*-}
-    fi
 
     fix_amdsmi_version "$version"
 
@@ -111,11 +80,8 @@ function clone_and_fix_amdsmi {
     git push origin "$version"
 }
 
-# AMDSMI Tags
-AMDSMI_TAGS_URL="https://api.github.com/repos/ROCm/amdsmi/tags"
-
-# Fetch the JSON data
-tags_json=$(curl -s "$AMDSMI_TAGS_URL")
+# AMDSMI Latest Release
+AMDSMI_RELEASE_URL="https://api.github.com/repos/ROCm/amdsmi/releases/latest"
 
 # Check if 'jq' is installed
 if ! command -v jq &> /dev/null; then
@@ -123,38 +89,19 @@ if ! command -v jq &> /dev/null; then
     exit 1
 fi
 
-# Extract tag names and commit SHAs into an array
-mapfile -t tags_info < <(echo "$tags_json" | jq -c '.[] | {name: .name, sha: .commit.sha}')
+# Associated tag with release (ex. rocm-6.2.1)
+tag_name=$(curl -s $AMDSMI_RELEASE_URL | jq -r '.tag_name')
 
-# Initialize an array to hold tags with their commit dates
-declare -a tags_with_dates
+# Extract out the version number (ex. 6.2.1)
+version_number=$(echo "$tag_name" | sed 's/^rocm-//')
 
-# Process each tag to get the commit date and SHA
-for tag in "${tags_info[@]}"; do
-    tag_name=$(echo "$tag" | jq -r '.name')
-    commit_sha=$(echo "$tag" | jq -r '.sha')
+# Call tag_already_published function to check if tag has been published.
+if ! tag_already_published "$version_number"; then
+    echo "New tag detected: $tag_name. Building and pushing python wrapper."
 
-    # Fetch the commit data
-    commit_json=$(curl -s "https://api.github.com/repos/ROCm/amdsmi/commits/$commit_sha")
-    commit_date=$(echo "$commit_json" | jq -r '.commit.committer.date')
+    # retrive commit_sha from Github tags API
+    commit_hash=$(curl -s https://api.github.com/repos/ROCm/amdsmi/git/ref/tags/$tag_name | jq -r '.object.sha')
 
-    # Append the data to the array
-    tags_with_dates+=("$commit_date $tag_name $commit_sha")
-done
-
-# Sort the tags by commit date from oldest to newest
-IFS=$'\n' sorted_tags=($(sort <<<"${tags_with_dates[*]}"))
-unset IFS
-
-# Loop through the sorted tags
-for tag_info in "${sorted_tags[@]}"; do
-    # Read the commit date, tag name, and commit SHA
-    IFS=' ' read -r commit_date tag_name commit_sha <<< "$tag_info"
-
-    # Call tag_already_published function
-    if ! tag_already_published "$tag_name"; then
-        echo "New tag detected: $tag_name. Fixing and pushing python wrapper."
-        # If tag not already published, call clone_and_fix_amdsmi
-        clone_and_fix_amdsmi "$tag_name" "$commit_sha"
-    fi
-done
+    # If tag not already published, call clone_and_fix_amdsmi
+    clone_and_fix_amdsmi "$commit_hash" "$version_number"
+fi
