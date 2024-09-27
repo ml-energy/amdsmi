b96c7bf47b53f22aa75ff2cc27d8137ec30e08d6
Parth Raut <praut@umich.edu>
2024-09-26 23:22:44 -0400

    Add py-interface for version 5.5.0

--- py-interface/amdsmi/setup.py
+++ py-interface/amdsmi/setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("amdsmi/README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name='amdsmi',
-    version='1.0.0+rocm-5.5.0',
+    version='5.5.0',
     description="SMI LIB - AMD GPU Monitoring Library",
     long_description=long_description,
     long_description_content_type="text/markdown",
